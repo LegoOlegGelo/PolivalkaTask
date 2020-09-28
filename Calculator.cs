0ce3f29b490e2ae55d1db1bc5c0012dc223e83a6
Oleg Akhmetkin <leviter517@gmail.com>
2020-09-28 17:45:01 +0300

    Иправлена ошибка определения угла в 3 и 4 коорд. четвертях

--- Calculator.cs
+++ Calculator.cs
@@ -89,7 +89,7 @@ namespace PolivalkaTask
 				(true, false) => 4
 			};
 
-			const double x1 = 1, y1 = 0; // координаты вектора отсчета (1;0)
+			const double x1 = 1, y1 = 0; // координаты вектора отсчета (0;1)
 
 			var vectorMultiple = x1 * dx + y1 * dy;
 			var scalarMultiple1 = Math.Sqrt(x1 * x1 + y1 * y1);
